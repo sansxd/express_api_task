ca3a0efa4deadef9b52970f011780974f37911d0
Sergion Nuñez Sanchez <sergioans95@hotmail.com>
2019-08-12 18:04:28 -0400

    fixs files and add Readme

---
+++ README.md
@@ -0,0 +1,3 @@
+"# express_api_task"
+npm install
+npm run dev
--- src/database.js
+++ src/database.js
@@ -1,17 +1,7 @@
-const MongoClient = require('mongodb').MongoClient;
 import mongoose from 'mongoose';
 import { uri } from './config';
 
-//url con conexion a la bd
-// const uri =
-//   'mongodb+srv://sergio:sans@mongo-lico-yjw0p.azure.mongodb.net/test?retryWrites=true&w=majority';
-
 export async function conexion() {
-  // try {
-  // mongoose
-  //   .connect(uri, { useNewUrlParser: true })
-  //   .catch(error => handleError(error));
-
   try {
     await mongoose.connect(uri, {
       useNewUrlParser: true
@@ -20,28 +10,4 @@ export async function conexion() {
   } catch (error) {
     handleError(error);
   }
-  // const client = await MongoClient.connect(uri, {
-  //   useNewUrlParser: true
-  // });
-  // const database = client.db('testeo');
-
-  // console.log('Conectado a mongodb-Cloud-Atlas!');
-  // return database;
-  // client.connect(err => {
-  //   const collection = client.db('testeo');
-  //   // perform actions on the collection object
-  //   // client.close();
-  //   if (err) {
-  //     console.log('paso por error ', { err });
-
-  //     client.close();
-  //   } else {
-  //     console.log('Conectado a mongodb-Cloud-Atlas!');
-  //   }
-
-  //   return collection;
-  // });
-  // } catch (e) {
-  //   console.log(`desde database.js: ${e}`);
-  // }
 }
